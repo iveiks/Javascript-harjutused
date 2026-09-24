6efdddf89e9f7f317ab2f4573b4f08cad48e681d
iveiks <vlemberg@hkhk.edu.ee>
2026-09-24 16:26:59 +0300

    harjutus13-14

---
+++ harj13.js
@@ -0,0 +1,33 @@
+const kaardid = document.querySelectorAll(".card")
+const pealkiri = document.querySelectorAll("h5")
+const kirj = document.querySelectorAll("p")
+
+const loend = document.querySelectorAll("li")
+
+loend.forEach(e => {
+    if(e.textContent.includes("Ootel")){
+        e.classList.add("list-group-item-warning")
+    }
+});
+
+loend.forEach(e => {
+    if(e.textContent.includes("Tehtud")){
+        e.classList.add("list-group-item-success")
+    }
+});
+
+loend.forEach(e => {
+    if(e.textContent.includes("Viga")){
+        e.classList.add("list-group-item-danger")
+    }
+});
+
+let m = 0
+kaardid.forEach(kaart => {
+    
+    let nimi = kaart.querySelector("img").getAttribute("data-title")
+    let kirjeldus = kaart.querySelector("img").getAttribute("data-description")
+    pealkiri[m].innerHTML=nimi
+    kirj[m].innerHTML=kirjeldus
+    m+=1
+});
